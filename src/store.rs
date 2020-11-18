d3235e26fdf262df2be7fac78729f791fc0d565d
rezzett <rezzett@mail.ru>
2020-11-18 22:23:21 +0600

    struct Store added.Store's inetrface prototype created

--- src/store.rs
+++ src/store.rs
@@ -10,3 +10,33 @@ impl Note {
         Note {text: text.to_string()}
     }
 }
+
+pub struct Store {
+    notes: Vec<Note>
+}
+
+impl Store {
+    pub fn new() -> Store {
+        Store {notes: vec![]}
+    }
+
+    pub fn add_note(&mut self, text: &str) {
+        unimplemented!();
+    }
+
+    pub fn remove_note(&mut self, index: usize) {
+        unimplemented!();
+    }
+
+    pub fn get_notes(&self) -> Vec<Note> {
+        unimplemented!();
+    }
+
+    fn save(&self) {
+        unimplemented!();
+    }
+
+    fn read(&mut self) {
+        unimplemented!();
+    }
+}
