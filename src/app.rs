04775c64d0996fd11ea1283d483ff925faf1bf66
rezzett <rezzett@mail.ru>
2020-11-18 21:57:32 +0600

    refactor command_handler

--- src/app.rs
+++ src/app.rs
@@ -1,19 +1,20 @@
-
 enum AppState {
     Menu,
     Add,
     Delelte,
     Show,
-    Quit
+    Quit,
 }
 
 pub struct App {
-    state: AppState
+    state: AppState,
 }
 
 impl App {
     pub fn new() -> App {
-        App {state: AppState::Menu}
+        App {
+            state: AppState::Menu,
+        }
     }
 
     pub fn run(&mut self) {
@@ -23,22 +24,22 @@ impl App {
                     App::menu();
                     let user_input = App::get_input("Choose command:");
                     self.command_handler(user_input.as_str());
-                },
+                }
                 AppState::Add => {
                     println!("ADD");
                     self.state = AppState::Menu;
-                },
+                }
                 AppState::Delelte => {
                     println!("DEL");
                     self.state = AppState::Menu;
-                },
+                }
                 AppState::Show => {
                     println!("SHOW");
                     self.state = AppState::Menu;
-                },
+                }
                 AppState::Quit => {
                     break;
-                },
+                }
             }
         }
     }
@@ -53,24 +54,24 @@ impl App {
     }
 
     fn command_handler(&mut self, user_input: &str) {
-        if user_input == "a" {
-            self.state = AppState::Add;
-        }
-        if user_input == "d" {
-            self.state = AppState::Delelte;
-        }
-        if user_input == "s" {
-            self.state = AppState::Show;
-        }
-        if user_input == "q" {
-            self.state = AppState::Quit;
+        match user_input {
+            "a" => self.state = AppState::Add,
+            "d" => self.state = AppState::Delelte,
+            "s" => self.state = AppState::Show,
+            "q" => self.state = AppState::Quit,
+            _ => {
+                println!("Invalid command!");
+                self.state = AppState::Menu;
+            }
         }
     }
 
     fn get_input(prompt: &str) -> String {
         println!("{}", prompt);
         let mut buf = String::new();
-        std::io::stdin().read_line(&mut buf).expect("[ERROR]: Input failed at function app::App:get_input");
+        std::io::stdin()
+            .read_line(&mut buf)
+            .expect("[ERROR]: Input failed at function app::App::get_input");
         buf.trim().to_string()
     }
 }
